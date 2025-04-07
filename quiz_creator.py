682976cee9bed226fbba3d1a2866e7c3386d91bd
DyrenJmsu <darenjamesdedios@gmail.com>
2025-04-08 07:42:28 +0800

    Added while loop so that it continouosly ask the user to create a question

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,12 +1,13 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
-#Asking the user to write a question
-print("Please enter your quiz question")
-question = input("Q: ") 
-#Asking the user to write 4 possible answer
-print("Please input the 4 possible answer")
-a = input("For letter a: ")
-b = input("For letter b: ")
-c = input("For letter c: ")
-d = input("For letter d: ")
-#Asking for the correct answer
-correct_answer = input("The correct answer?(is it a, b, c, or d?): ")
+while True:
+    #Asking the user to write a question
+    print("Please enter your quiz question")
+    question = input("Q: ") 
+    #Asking the user to write 4 possible answer
+    print("Please input the 4 possible answer")
+    a = input("For letter a: ")
+    b = input("For letter b: ")
+    c = input("For letter c: ")
+    d = input("For letter d: ")
+    #Asking for the correct answer
+    correct_answer = input("The correct answer?(is it a, b, c, or d?): ")
