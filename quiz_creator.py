85e5ff526cbfc1cfd6998d6357f8312960747474
DyrenJmsu <darenjamesdedios@gmail.com>
2025-04-08 11:59:22 +0800

    Added colors to some strings to make it more visible and creative using ANSI escape sequence

--- quiz_creator.py
+++ quiz_creator.py
@@ -4,10 +4,12 @@ import os
 
 filename =  "quiz_questions.txt"
 
+print("Welcome to \033[1m\033[33mQuiz Creator\033[0m") #Added color for a bit of effects hehe
+
 while True:
     #Asking the user to write a question
     print("Please enter your quiz question.")
-    questions = input("Q: ")
+    questions = input("\033[33mQ\033[0m: ")
 
     #Asking the user to write 4 possible answer
     print("Please input the 4 possible answer.")
@@ -20,10 +22,10 @@ while True:
     while True:
         correct_answer = input("The correct answer?(is it a, b, c, or d?): ")
         if correct_answer in ['a', 'b', 'c', 'd']:
-            print("Saving the question and answer")
+            print("\033[32mSaving the question and answer\033[0m")
             break
         else:
-            print("Invalid input, please choose only a, b, c, or d")
+            print("\033[31mInvalid input, please choose only a, b, c, or d\033[0m")
 
     #A program that saves the quiz questions into a file
     with open(filename, "a", encoding="utf-8") as file:
@@ -33,15 +35,16 @@ while True:
         file.write(f"c. {c}\n")
         file.write(f"d. {d}\n")
         file.write(f"Correct Answer: {correct_answer}\n\n")
+        file.write("=" * 50 + "\n\n")
 
     #Asking the user if they want to stop adding more quiz questions
     while True:
         print("Do you want to add another quiz question?")
-        respond = input("yes or no?: ").lower()
+        respond = input("\033[32myes\033[0m or \033[31mno\033[0m?: ").lower()
         if respond == 'yes':
             break
         elif respond == 'no':
-            print("Exiting, your quiz questions are now saved.")
+            print("Exiting, your quiz questions are now \033[32msaved\033[0m.")
             exit()
         else:
-            print("Only yes or no answer are allowed my friend.")
+            print("Only \033[32myes\033[0m or \033[31mno\033[0m answer are allowed my friend.")
