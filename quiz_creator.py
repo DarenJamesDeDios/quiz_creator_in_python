f6be9ea818d099631e3f4d6ade38db968e62e4f1
DyrenJmsu <darenjamesdedios@gmail.com>
2025-04-08 10:50:55 +0800

    Trying to learn how to create a file using os module and trying to save every question and answer inputed by the user using with open as file

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,8 +1,13 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+#Importing a module for saving purposes
+import os
+
+filename =  "quiz_questions.txt"
+
 while True:
     #Asking the user to write a question
     print("Please enter your quiz question.")
-    question = input("Q: ") 
+    questions = input("Q: ") 
     #Asking the user to write 4 possible answer
     print("Please input the 4 possible answer.")
     a = input("For letter a: ")
@@ -19,6 +24,14 @@ while True:
             break
         elif respond == 'no':
             print("Exiting, your quiz questions are now saved.")
+            #A program that saves the quiz questions into a file
+            with open(filename, "a", encoding="utf-8") as file:
+                file.write(f"Q: {questions}\n")
+                file.write(f"a. {a}\n")
+                file.write(f"b. {b}\n")
+                file.write(f"c. {c}\n")
+                file.write(f"d. {d}\n")
+                file.write(f"Correct Answer: {correct_answer}\n\n")
             exit()
         else:
             print("Only yes or no answer are allowed my friend.")
