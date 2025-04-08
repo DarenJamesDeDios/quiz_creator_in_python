90e10870107d6a645907f61042021404fac289ce
DyrenJmsu <darenjamesdedios@gmail.com>
2025-04-08 13:49:47 +0800

    Added time library for incorporating time on the program, handling delays which makes the user have the time to read the strings

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,10 +1,12 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
 #Importing a module for saving purposes
 import os
+import time  #Importing time for delays of printed strings 
 
 filename =  "quiz_questions.txt"
 
 print("Welcome to \033[1m\033[33mQuiz Creator\033[0m") #Added color for a bit of effects hehe
+time.sleep(2)
 
 while True:
     #Asking the user to write a question
@@ -22,10 +24,14 @@ while True:
     while True:
         correct_answer = input("The correct answer?(is it a, b, c, or d?): ")
         if correct_answer in ['a', 'b', 'c', 'd']:
-            print("\033[32mSaving the question and answer\033[0m")
+            print("\033[33mSaving the question and answer...\033[0m")
+            time.sleep(3)
+            print("\033[32mDone!\033[0m")
+            time.sleep(1)
             break
         else:
             print("\033[31mInvalid input, please choose only a, b, c, or d\033[0m")
+            time.sleep(1)
 
     #A program that saves the quiz questions into a file
     with open(filename, "a", encoding="utf-8") as file:
@@ -44,7 +50,10 @@ while True:
         if respond == 'yes':
             break
         elif respond == 'no':
-            print("Exiting, your quiz questions are now \033[32msaved\033[0m.")
+            print("\033[33mExiting...\033[0m")
+            time.sleep(3)
+            print("Your quiz questions are now \033[32msaved\033[0m.")
             exit()
         else:
             print("Only \033[32myes\033[0m or \033[31mno\033[0m answer are allowed my friend.")
+            time.sleep(1)
