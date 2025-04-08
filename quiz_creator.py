238285d5daea8ddade78fbb87429628d6c421827
DyrenJmsu <darenjamesdedios@gmail.com>
2025-04-08 14:45:52 +0800

    Removed the os module as it is no use to me, forgot to remove it while testing file handling hehe

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,7 +1,5 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
-#Importing a module for saving purposes
-import os
-import time  #Importing time for delays of printed strings 
+import time  #Importing time for delays of printed strings .
 
 filename =  "quiz_questions.txt"
 
