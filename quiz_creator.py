67780ac1b2f359e0bcdca6668f0ee11ddf92cc6a
DyrenJmsu <darenjamesdedios@gmail.com>
2025-04-08 08:22:29 +0800

    Added another while loop for the user choice if they still want to add another quiz question and I managed to get the intended output

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,10 +1,10 @@
 #Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
 while True:
     #Asking the user to write a question
-    print("Please enter your quiz question")
+    print("Please enter your quiz question.")
     question = input("Q: ") 
     #Asking the user to write 4 possible answer
-    print("Please input the 4 possible answer")
+    print("Please input the 4 possible answer.")
     a = input("For letter a: ")
     b = input("For letter b: ")
     c = input("For letter c: ")
@@ -12,11 +12,13 @@ while True:
     #Asking for the correct answer
     correct_answer = input("The correct answer?(is it a, b, c, or d?): ")
     #Asking the user if they want to stop adding more quiz questions
-    print("Do you want to add another quiz question?")
-    respond = input("yes or no?: ").lower
-    if respond == "yes":
-        continue
-    elif respond == "no":
-        break
-    else:
-        print("Only yes or no answer are allowed my friend")
+    while True:
+        print("Do you want to add another quiz question?")
+        respond = input("yes or no?: ").lower()
+        if respond == 'yes':
+            break
+        elif respond == 'no':
+            print("Exiting, your quiz questions are now saved.")
+            exit()
+        else:
+            print("Only yes or no answer are allowed my friend.")
